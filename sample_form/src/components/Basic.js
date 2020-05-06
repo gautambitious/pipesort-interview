0bd8b58d6c49437e2c9b528019737b782babae26
gautambitious <mvgt.gautam@gmail.com>
2020-05-07 04:28:14 +0530

    Experience

--- sample_form/src/components/Basic.js
+++ sample_form/src/components/Basic.js
@@ -18,21 +18,21 @@ export const Basic = ({nextPage}) =>
         nextPage()
     }
         return (
-          <main className="pa4 black-80">
+          <main className="pa4 black-80 shadow-1 mt3">
               <h1 className="measure center">Internship Application</h1>
               <form className="mt3" id="basic-form">
                   <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                   <div className="mt2">
                       <label className="db fw6 lh-copy f6">First Name</label>
-                      <input onChange={(event) => setFirstName(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" id="firstname" type="text" placeholder="Michael"/>
+                      <input onChange={(event) => setFirstName(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" id="firstname" type="text" placeholder="Michael"/>
                   </div>
                   <div className="mt2">
                       <label className="db fw6 lh-copy f6">Last Name</label>
-                      <input onChange={(event) => setLastName(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" type="text" id="lastname" placeholder="Scott"/>
+                      <input onChange={(event) => setLastName(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" type="text" id="lastname" placeholder="Scott"/>
                   </div>
                   <div className="mt2">
                       <label className="db fw6 lh-copy f6">Email</label>
-                      <input onChange={(event) => setEmail(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" type="email" id="email" placeholder="michael@dundermifflin.com"/>
+                      <input onChange={(event) => setEmail(event.target.value)} className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" type="email" id="email" placeholder="michael@dundermifflin.com"/>
                   </div>
                   </fieldset>
                   <input value="Save" type="submit" onClick={basicSubmit} className="grow pointer b--solid b--black-20 bg-white black pa2 b"/>
--- sample_form/src/components/Experience.js
+++ sample_form/src/components/Experience.js
@@ -1,27 +1,27 @@
 import React, {useState} from "react";
 
 export const Experience = ({nextStep, prevStep}) => {
-
+    const [resume, setResume] = useState("");
     return (
-        <main className="pa4 black-80">
-            <h1 className="measure center">Internship Application</h1>
+        <main className="pa4 black-80 shadow-1 mt3">
+            <h1 className="measure center ph5">Experience</h1>
             <form className="mt3" id="basic-form">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <div className="mt2">
-                        <label className="db fw6 lh-copy f6">First Name</label>
-                        <input  className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" id="resumelink" type="text" placeholder="Michael"/>
+                        <label className="db fw6 lh-copy f6">Link to Resume</label>
+                        <input  className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" id="resumelink" onChange={(event) => setResume(event.target.value)} type="url" placeholder="" />
                     </div>
                     <div className="mt2">
                         <label className="db fw6 lh-copy f6">Last Name</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" type="text" id="lastname" placeholder="Scott"/>
+                        <textarea className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" type="text" id="lastname" placeholder="Scott"/>
                     </div>
                     <div className="mt2">
                         <label className="db fw6 lh-copy f6">Email</label>
-                        <input className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue hover-white w-100" type="email" id="email" placeholder="michael@dundermifflin.com"/>
+                        <textarea className="pa2 input-reset ba bg-transparent hover-bg-lightest-blue w-100" type="email" id="email" placeholder="michael@dundermifflin.com"/>
                     </div>
                 </fieldset>
                 <div>
-                    <input value="Save" type="submit" onClick={prevStep()} className="grow pointer b--solid b--black-20 bg-white black pa2 b"/>
+                    <input value="Save" type="submit" onClick={prevStep} className="grow pointer b--solid b--black-20 bg-white black pa2 b"/>
                 </div>
             </form>
         </main>
